df784d8cda1f36992cc5b9a27c3a3b9bb0955138
Joao Carlos Casa <joaocarlos.exe@gmail.com>
2022-05-03 00:32:44 -0300

    alura curse android 1, cap 4

---
+++ Alura/1-PrimeiroApp/app/src/main/java/com/agendaJC/agenda/ui/activity/studentFormActivity.java
@@ -0,0 +1,16 @@
+package com.agendaJC.agenda.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.agendaJC.agenda.R;
+
+public class studentFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_form);
+    }
+}
--- Alura/1-PrimeiroApp/app/src/main/java/com/agendaJC/agenda/ui/activity/studentListActivity.java
+++ Alura/1-PrimeiroApp/app/src/main/java/com/agendaJC/agenda/ui/activity/studentListActivity.java
@@ -1,26 +1,28 @@
-package com.agendaJC.agenda;
+package com.agendaJC.agenda.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.agendaJC.agenda.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class studentListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        setTitle("Student List");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_student_list);
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("carss", "er", "ta"));
-        ListView listaDeAlunos = findViewById(R.id.activity_main_student_list);
+        ListView listaDeAlunos = findViewById(R.id.activity_student_list_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this, android.R.layout.simple_list_item_1, alunos
                 )
